9796b38a1135595dc319b0f6c4f5ee92c3eccd1d
jcbarbaf <jcbarbaf@gmail.com>
2023-11-30 14:06:29 +0100

    snaps

--- script/piece.js
+++ script/piece.js
@@ -1,5 +1,6 @@
 export default (() => {
   let piece = document.querySelector('.piece');
+  let mouseFollower = document.querySelector('.mouse-follower')
   let pieceClone;
   let map = document.querySelector('.map');
   piece.addEventListener('click',() => {
@@ -7,20 +8,19 @@ export default (() => {
     //Clone the piece
     pieceClone = piece.cloneNode(true);
     pieceClone.classList.add('clone');
-    document.body.appendChild(pieceClone);
-    let isrotated = false;
+    mouseFollower.appendChild(pieceClone);
     // On right click rotate the piece 90deg
     pieceClone.addEventListener('contextmenu', (event) => {
       event.preventDefault();
       // degrees += 90;
-      if (isrotated) {
-        pieceClone.style.transform= 'rotate(0deg)';
-        isrotated = false;
-      } else {
-        pieceClone.style.transform= 'rotate(90deg)';
-        isrotated = true;
-      }
-      console.log(`rotate(${pieceClone.style.transform+90}deg)`);
+      pieceClone.classList.toggle('rotated')
+      // if (isrotated) {
+      //   pieceClone.style.transform= 'rotate(0deg)';
+      //   isrotated = false;
+      // } else {
+      //   pieceClone.style.transform= 'rotate(90deg)';
+      //   isrotated = true;
+      // }
     });
     // On regular click place the piece
     pieceClone.addEventListener('click', () => {
@@ -33,11 +33,9 @@ export default (() => {
   //Move the piece with the mouse
   document.addEventListener('mousemove',(event) => {
     // console.log(piece.style);
-    if (pieceClone) {
-      pieceClone.style.top = (event.clientY-(pieceClone.offsetHeight*0.5)) + "px";
-      pieceClone.style.left = (event.clientX-(pieceClone.offsetWidth*0.25)) + "px";
-      // pieceClone.style.top = (event.clientY-(pieceClone.offsetHeight*1)) + "px";
-      // pieceClone.style.left = (event.clientX-(pieceClone.offsetWidth*1)) + "px";
-    }
-  });
+    mouseFollower.style.top = (event.clientY-(mouseFollower.offsetHeight*0.5)) + "px";
+    mouseFollower.style.left = (event.clientX-(mouseFollower.offsetWidth*0.25)) + "px";
+    // pieceClone.style.top = (event.clientY-(pieceClone.offsetHeight*1)) + "px";
+    // pieceClone.style.left = (event.clientX-(pieceClone.offsetWidth*1)) + "px";
+});
 })();
