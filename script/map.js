eb7d74f2d805dccb1272b5e87ab60a4664bf94a6
jcbarbaf <jcbarbaf@gmail.com>
2023-12-01 15:52:00 +0100

    mapa rando

--- script/map.js
+++ script/map.js
@@ -1,80 +1,69 @@
 export default (() => {
     let squares = document.querySelectorAll('.map .square');
+    let mouseFollower = document.querySelector('.mouse-follower');
     let snapPoints = document.querySelectorAll('.map .square .snap-point');
-    let template = [
-        1,1,1,0,0,0,0,
+    let templates = [
+        [1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,],
+        [1,0,0,0,0,0,1,
+        1,1,1,0,1,1,1,
+        1,1,1,0,1,1,1,
+        0,1,1,1,1,1,0,
+        0,1,1,1,1,1,0,
+        1,1,1,0,1,1,1,
+        1,1,0,0,0,1,1,],
+        [1,1,1,0,0,0,0,
         1,1,1,1,0,0,0,
         1,1,1,0,0,0,0,
         1,1,1,1,1,1,1,
         0,1,1,1,1,1,0,
         1,1,1,1,1,1,0,
-        1,1,1,1,0,1,1,
+        1,1,1,1,0,1,1,]
     ];
     let positions = [{}];
-    for (let i = 0; i < template.length; i++) {
-        if (template[i] == 1) {
+    let randomIndex = Math.floor(Math.random() * templates.length);
+    randomIndex = 1;
+    for (let i = 0; i < templates[randomIndex].length; i++) {
+        if (templates[randomIndex][i] == 1) {
             squares[i].classList.add('active');
         }
     }
     for (let i = 0; i < squares.length; i++) {
         if (squares[i].classList.contains('active')) {
             let position = snapPoints[i].getBoundingClientRect();
-            positions[i] = {'x': position.top, 'y': position.left};      
+            positions[i] = {'x': position.x, 'y': position.y};      
         }
     }
-    // console.log(positions);
     document.addEventListener('placePiece', (event => {
         let piece = document.querySelector('.snap-point .piece.clone');
         piece.classList.remove('clone');
-        // console.log(`X = ${positions[1]['x']} , Y = ${positions[1]['y']}`);
-        // piece.style.top = positions[0]['x'] + "px";
-        // piece.style.left = positions[0]['y'] + "px";
-        // console.log(piece);
-
-        snapPoints[0].appendChild(piece);
-        // findClosestPosition(event);
-        var closestPosition = positions.reduce(function (closest, current) {
-            var distanceToCurrent = calculateDistance({ x: mouseX, y: mouseY }, current);
-            var distanceToClosest = calculateDistance({ x: mouseX, y: mouseY }, closest);
-            return distanceToCurrent < distanceToClosest ? current : closest;
-        }, positions[0]); // Start with the first position as the initial closest
-        console.log(closestPosition)
+        // todo rellenar las posiciones vacias con ""
+        // todo que solo se ponga en una posicion si esta a menos de X pixeles de distancia 
+        let mouseRect = mouseFollower.getBoundingClientRect();
+        let smallestDistance = calculateDistance(positions[0],{ x: mouseRect.x, y: mouseRect.y });
+        let smallestDistanceIndex = 0;
+        positions.forEach((snapPoint,i) => {
+            let distance = calculateDistance(snapPoint,{ x: mouseRect.x, y: mouseRect.y });
+            if (distance < smallestDistance) {
+                smallestDistance = distance;
+                smallestDistanceIndex = i;
+            };
+        });
+        snapPoints[smallestDistanceIndex].appendChild(piece);
+        //------------------------------------------------
+        // console.log(positions[smallestDistanceIndex]);
+        // positions.splice(smallestDistanceIndex,1);
+        // console.log(positions[smallestDistanceIndex]);
+        //------------------------------------------------
     }));
     function calculateDistance(point1, point2) {
         var dx = point1.x - point2.x;
         var dy = point1.y - point2.y;
         return Math.sqrt(dx * dx + dy * dy);
     };
-  
-    // Function to find the closest position and perform an action
-    // function findClosestPosition(event) {
-    //     var mouseX = event.clientX;
-    //     var mouseY = event.clientY;
-  
-        // Find the closest position
-        // var closestPosition = positions.reduce(function (closest, current) {
-        //   var distanceToCurrent = calculateDistance({ x: mouseX, y: mouseY }, current);
-        //   var distanceToClosest = calculateDistance({ x: mouseX, y: mouseY }, closest);
-        //   return distanceToCurrent < distanceToClosest ? current : closest;
-        // }, positions[0]); // Start with the first position as the initial closest
-  
-        // Log the closest position
-        // for (let i = 0; i < positions.length; i++) {
-        //     if (positions[i]) {
-        //         console.log(`Positions de index: X=${positions[i]['x']} Y=${positions[i]['y']} | closest position: X=${closestPosition['x']} Y=${closestPosition['y']} `)
-        //     }
-            
-        // }
-    //}
-    // console.log(pos)
-    // console.log('Top:', pos.top);
-    // console.log('Left:', pos.left);
-    // console.log('Bottom:', pos.bottom);
-    // console.log('Right:', pos.right);
-    // let test = document.createElement('div');
-    // test.classList.add('test');
-    // test.style.top = pos.top + "px";
-    // test.style.left = pos.left + "px";
-    // document.body.appendChild(test)
-
   })();
