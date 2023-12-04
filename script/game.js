18a542c413547b486e74d2fa12ad80137168ffcd
jcbarbaf <jcbarbaf@gmail.com>
2023-12-04 15:52:10 +0100

    borrar piezas (WIP)

--- script/game.js
+++ script/game.js
@@ -1,9 +1,10 @@
 export default (() => {
-    let squares = document.querySelectorAll('.map .square');
-    let mouseFollower = document.querySelector('.mouse-follower');
-    let snapPoints = document.querySelectorAll('.map .square .snap-point');
-    let piece = document.querySelector('.piece');
-    let counterDisplay = document.querySelector('.piece-counter');
+    const squares = document.querySelectorAll('.map .square');
+    const mouseFollower = document.querySelector('.mouse-follower');
+    const snapPoints = document.querySelectorAll('.map .square .snap-point');
+    const piece = document.querySelector('.piece');
+    const counterDisplay = document.querySelector('.piece-counter');
+    const winModal = document.querySelector('.modal-container.win');
     let pieceClone;
     let templates = [
         {
@@ -16,6 +17,16 @@ export default (() => {
                 1, 1, 1, 1, 1, 1, 1,
                 1, 1, 1, 1, 1, 1, 1,
                 1, 1, 1, 1, 1, 1, 1,]},
+        {
+            piecesUsed: 4,
+            template:
+                [1, 1, 0, 0, 0, 0, 0,
+                0, 1, 1, 1, 1, 0, 0,
+                1, 1, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0,]},
         {
             piecesUsed: 17,
             template:
@@ -56,20 +67,11 @@ export default (() => {
                 1, 0, 0, 1, 0, 0, 1,
                 1, 0, 0, 1, 0, 0, 1,
                 1, 1, 0, 0, 0, 1, 1,]},
-        {
-            piecesUsed: 4,
-            template:
-                [1, 1, 0, 0, 0, 0, 0,
-                0, 1, 1, 1, 1, 0, 0,
-                1, 1, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0,]},
     ];
     let positions = [{}];
-    let randomIndex = Math.floor(Math.random() * templates.length);
-    randomIndex = 5;
+    // let randomIndex = Math.floor(Math.random() * templates.length);
+    let randomIndex = Math.floor(Math.random() * ((templates.length-1) - 2 + 1) + 2)
+    // randomIndex = 5;
     let counter = templates[randomIndex]['piecesUsed'];
     counterHandler();
     // Generate map using the template
@@ -153,6 +155,12 @@ export default (() => {
     function placePiece(pieceIndex, isrotated) {
         //Place piece
         pieceClone.classList.add('placed');
+        // todo quitar evento de las piezas clonadas
+        // pieceClone.removeEventListener('contextmenu');
+        pieceClone.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
+            deletePiece(event.target.closest('.piece'));
+        });
         snapPoints[pieceIndex].appendChild(pieceClone);
         //Delete the snap-points in use
         positions[pieceIndex] = null;
@@ -165,7 +173,16 @@ export default (() => {
         counter--;
         counterHandler();
     }
+    function deletePiece(piece) {
+        piece.remove();
+        counter++;
+        counterHandler();
+    }
     function counterHandler() {
         counterDisplay.innerHTML = `${counter} pieces`;
+        if (counter == 0) {
+            console.log('hola');
+            winModal.classList.add('active');
+        }
     }
 })();
